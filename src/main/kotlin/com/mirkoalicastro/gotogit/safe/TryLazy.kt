67458b759fd993f69fbb5dae995a0df245136eb7
mirkoalicastro <mirkoalicastro@gmail.com>
2021-04-29 22:52:44 +0200

    Logs exception if thrown

--- src/main/kotlin/com/mirkoalicastro/gotogit/safe/TryLazy.kt
+++ src/main/kotlin/com/mirkoalicastro/gotogit/safe/TryLazy.kt
@@ -1,14 +1,17 @@
 package com.mirkoalicastro.gotogit.safe
 
+import com.mirkoalicastro.gotogit.log.Logging
+import com.mirkoalicastro.gotogit.log.logger
 import kotlin.reflect.KProperty
 
 fun <T> tryLazy(initializer: () -> T?) = TryLazy(initializer)
 
-class TryLazy<T>(initializer: () -> T?) {
+class TryLazy<T>(initializer: () -> T?): Logging {
     private val value: T? by lazy {
         try {
             initializer()
         } catch (e: Exception) {
+            logger().error("Failed to initialize.", e)
             null
         }
     }
